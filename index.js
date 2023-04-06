ca1af9496fb5a9201f514dc35a250d0a38d25cef
Thiago Santos <49589136+ThiagoSzz@users.noreply.github.com>
2023-04-06 18:17:43 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ import App from "./App";
 AppRegistry.registerComponent("App", () => App);
 
 AppRegistry.runApplication("App", {
-  rootTag: document.getElementById("react-root")
+  rootTag: document.getElementById("root")
 });
